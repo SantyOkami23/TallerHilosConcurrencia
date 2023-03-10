93941b8d824deb7ed8fbff861b28e07157dd1561
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2023-03-10 18:00:51 -0400

    Capitulo 12 Java

---
+++ src/main/java/org/example/Sincronized/PracticeThread.java
@@ -0,0 +1,11 @@
+package org.example.Sincronized;
+
+public class PracticeThread extends Thread {
+    
+    @Override
+    public void run() {
+        PurchasingAgent agent = new PurchasingAgent();
+        agent.purchase();
+    }
+    
+}
